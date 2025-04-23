92bcbc649a32f15dcfcebc001de88769f069e87d
Michael2454555 <michaelyumbla6@gmail.com>
2025-04-23 10:45:13 -0500

    listo

--- src/app/components/fotos/fotos.component.ts
+++ src/app/components/fotos/fotos.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
-import { Firestore, collection, collectionData, CollectionReference } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Firestore, collection, getDocs, CollectionReference, query } from '@angular/fire/firestore';
 
 export interface Item {
   id?: string;
@@ -13,21 +12,29 @@ export interface Item {
   templateUrl: './fotos.component.html',
   styles: []
 })
-export class FotosComponent {
-  itemsCollection: CollectionReference<Item>;
-  items: Observable<Item[]>;
+export class FotosComponent implements OnInit {
+  private itemsCollection: CollectionReference<Item>;
+  public items: Item[] = [];
 
   constructor(private afs: Firestore) {
-    
     this.itemsCollection = collection(this.afs, 'img') as CollectionReference<Item>;
-
-    
-    this.items = collectionData(this.itemsCollection, { idField: 'id' }) as Observable<Item[]>;
-
-   
-    this.items.subscribe(arr => console.log('Fotos cargadas:', arr.length, arr));
   }
 
+  async ngOnInit(): Promise<void> {
+    try {
+      const q = query(this.itemsCollection);
+      const querySnap = await getDocs(q);
+
+      this.items = querySnap.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+
+      console.log('ITEMS:', this.items);
+    } catch (error) {
+      console.error('Error al obtener fotos:', error);
+    }
+  }
 
   onImgError(event: Event) {
     (event.target as HTMLImageElement).src = 'assets/img/placeholder.png';
